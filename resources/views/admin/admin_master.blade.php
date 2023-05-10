4a40f38a62e610250a56b0afe613a70383da3e20
RONNEL NAVARRO <ronnel.navarro2020@gmail.com>
2023-05-10 10:24:35 +0800

    test

---
+++ resources/views/admin/admin_master.blade.php
@@ -0,0 +1,9 @@
+@include('admin.body.header')
+
+@include('admin.body.navbar')
+
+@include('admin.body.sidebar')
+
+@yield('admin')
+        
+@include('admin.body.footer')
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\adminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::controller(adminController::class)->group(function () {
+    Route::get('admin/logout', 'destroy')->name('admin.logout');
+});
+
+require __DIR__.'/auth.php';
